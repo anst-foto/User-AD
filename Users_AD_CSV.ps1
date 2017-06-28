dffe45456edfb8a8ea01e76aacaf8ad269697186
Starinin Andrey <starininandrey@gmail.com>
2017-06-28 12:02:22 +0300

    Update Users_AD_CSV.ps1

--- Users_AD_CSV.ps1
+++ Users_AD_CSV.ps1
@@ -1,4 +1,24 @@
 Import-Module ActiveDirectory
+Clear-Host
+
+#*******************************************************
+Write-Host -ForegroundColor Yellow "*******************************************************"
+""
+Write-Host -ForegroundColor Yellow "Добавление пользователей в AD (PowerShell)"
+Write-Host -ForegroundColor Yellow "(c) AnSt. Март 2017"
+Write-Host -ForegroundColor Yellow "Версия: 0.3 (Июнь 2017)"
+""
+Write-Host -ForegroundColor Yellow "*******************************************************"
+""
+Write-Host -ForegroundColor Green "Изменения:
+v0.3 (Июнь 2017):   Добавление пользователя в соответствующую группу
+v0.2 (Июнь 2017):   Проверка: существует пользователь или нет
+v0.1 (Март 2017):   Создание скрипта"
+""
+Write-Host -ForegroundColor Yellow "*******************************************************"
+""
+#*******************************************************
+
 $Users = Import-Csv -Delimiter ";" -Path "C:\user.csv"
 $CN = "OU=Users,OU=Departament,DC=Organization,DC=local"
 Foreach ($User in $Users)
