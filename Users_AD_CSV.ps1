5783b079acb8dfbee3e255d3f21b24dd886b3abb
Starinin Andrey <starininandrey@gmail.com>
2017-07-26 17:19:06 +0300

    Обновление Users_AD_CSV.ps1
    Добавление информации в дополнительные поля
    Добавлены комментарии к коду

--- Users_AD_CSV.ps1
+++ Users_AD_CSV.ps1
@@ -6,11 +6,12 @@ Write-Host -ForegroundColor Yellow "********************************************
 ""
 Write-Host -ForegroundColor Yellow "Добавление пользователей в AD (PowerShell)"
 Write-Host -ForegroundColor Yellow "(c) AnSt. Март 2017"
-Write-Host -ForegroundColor Yellow "Версия: 0.3 (Июнь 2017)"
+Write-Host -ForegroundColor Yellow "Версия: 0.4 (Июнь 2017)"
 ""
 Write-Host -ForegroundColor Yellow "*******************************************************"
 ""
 Write-Host -ForegroundColor Green "Изменения:
+v0.4 (Июль 2017):	Добавление информации в дополнительные поля
 v0.3 (Июнь 2017):   Добавление пользователя в соответствующую группу
 v0.2 (Июнь 2017):   Проверка: существует пользователь или нет
 v0.1 (Март 2017):   Создание скрипта"
@@ -22,10 +23,14 @@ Write-Host -ForegroundColor Yellow "********************************************
 $Path = "C:\user.csv"                                                                   #Путь к файлу user.csv
 $Users = Import-Csv -Delimiter ";" -Path $Path                                          #Импорт csv-файла
 $CN = "OU=Users,OU=Departament,DC=Organization,DC=local"                                #Путь в AD
+$ADSI = "ADSI"																			#Имя дополнительного поля
+$ADSIData = "Data"																		#Значение дополнительного поля
+$ADSI2 = "ADSI2"																		#Имя дополнительного поля
+$ADSIData2 = "Data2"																	#Значение дополнительного поля
 Foreach ($User in $Users)
 {	
     $Password = $User.Password                                                          #Пароль пользователя
-    $Detailedname = $User.LastName + " " + $User.FirstName + " " + $User.MiddleName     #Полное имя
+    $DisplayName = $User.LastName + " " + $User.FirstName + " " + $User.MiddleName     #Полное имя
     $UserFirstName = $User.FirstName                                                    #Имя
     $UserLastName = $User.LastName                                                      #Фамилия
     $Department = $User.Department                                                      #Отдел
@@ -33,14 +38,19 @@ Foreach ($User in $Users)
     $Title = $User.Title                                                                #Должность
     $SAM= $User.Login + "@company.local"                                                #
     Try {
-            New-ADUser -Name $Detailedname -SamAccountName $User.Login -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $UserFirstName -Surname  $UserLastName -Company $Company -Department $Department -Title $Title  -AccountPassword  (ConvertTo-SecureString -AsPlainText $Password -Force) -PasswordNeverExpires $true -Enabled $true -Path $CN -Verbose
+			#Заведение нового пользователя в AD
+            New-ADUser -Name $DisplayName -SamAccountName $User.Login -UserPrincipalName $SAM -DisplayName $DisplayName -GivenName $UserFirstName -Surname  $UserLastName -Company $Company -Department $Department -Title $Title  -AccountPassword  (ConvertTo-SecureString -AsPlainText $Password -Force) -PasswordNeverExpires $true -Enabled $true -Path $CN -Verbose
         }
     Catch {
-            Set-ADUser -Identity "CN=$Detailedname,OU=Users,OU=Departament,DC=Organization,DC=local" -Company $Company -Department $Department -Title $Title -Verbose
+			#Если пользователь уже существует, то исправление его данных
+            Set-ADUser -Identity "CN=$DisplayName,OU=Users,OU=Departament,DC=Organization,DC=local" -Company $Company -Department $Department -Title $Title -Verbose
           }
-    Finally {    
+    Finally {
+		#Добавление пользователя к Группе пользователей на основе Отдела
         if ($Department -eq 'Departament1') {Add-ADGroupMember -Identity "Departament1" -Members $User.Login -Verbose}
         elseif ($Department -eq 'Departament2') {Add-ADGroupMember -Identity "Departament2" -Members $User.Login -Verbose}
         elseif ($Department -eq 'Departament3') {Add-ADGroupMember -Identity "Departament3" -Members $User.Login -Verbose}
+		#Добавление данных в дополнительные поля
+		Set-ADUser -Identity "CN=$DisplayName,OU=Users,OU=Departament,DC=Organization,DC=local" -Replace @{$ADSI=$ADSIData;$ADSI2=$ADSIData2} -Verbose
     }
 }
